179d884f9b35def02e038124cd6842939b4d9f77
vickyesantillan <vesantillan@live.com>
2021-05-17 15:47:54 -0300

    creating the function nextSequence, buttonColourds and gamePatterns variables

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert('working');
+var buttonColours = ['red', 'blue', 'green', 'yellow'];
+var gamePattern = [];
+
+function nextSequence() {
+  var randomNumberRange = Math.random() * 4;
+  var randomNumber = Math.floor(randomNumberRange);
+  var randomChosenColor = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColor);
+  console.log(gamePattern);
+}
+
+$('#yellow').click(nextSequence);
