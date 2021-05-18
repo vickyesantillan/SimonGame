312632aff90a8ec2d94aad36c65d0ac2a833a16f
vickyesantillan <vesantillan@live.com>
2021-05-18 10:35:31 -0300

    Adding game logic with the checkanswer function

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ var buttonColours = ['red', 'blue', 'green', 'yellow'];
 var gamePattern = [];
 var i = 0;
 var userClickedPattern = [];
+var level = 0;
+var start = false;
+
+//starting the game
+$(document).keydown(function () {
+  $('h1').text('Level ' + level);
+  nextSequence();
+  start = true;
+});
 
 //Sequence function, choose a color
 function nextSequence() {
+  $('h1').text('Level ' + level);
   var randomNumberRange = Math.random() * 4;
   var randomNumber = Math.floor(randomNumberRange);
   var randomChosenColor = buttonColours[randomNumber];
@@ -16,6 +26,7 @@ function nextSequence() {
     .fadeIn(100);
   playSound(randomChosenColor);
   i++;
+  level++;
 }
 
 //Play Sound function
@@ -24,10 +35,30 @@ function playSound(colorSound) {
   audio.play();
 }
 
+//Animate press function, add a class to the buttons
+function animatePress(currentColor) {
+  $('#' + currentColor).addClass('pressed');
+  setTimeout(function () {
+    $('#' + currentColor).removeClass('pressed');
+  }, 100);
+}
+
+//Game Logic
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === currentLevel) {
+      setTimeout(nextSequence(), 1000);
+      userClickedPattern = [];
+    }
+  } else {
+    console.log('wrong');
+  }
+}
+
 $('.btn').click(function () {
   var userChosenColor = this.id; //storing the button that the user clicked
   userClickedPattern.push(userChosenColor);
   playSound(userChosenColor);
-  console.log(userChosenColor);
-  console.log(userClickedPattern);
+  animatePress(userChosenColor);
+  checkAnswer(gamePattern.length);
 });
